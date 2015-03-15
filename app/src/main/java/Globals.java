5455410984d6302180577108abb750ce250c8747
RAWRRAWZ <ahung93@gmail.com>
2015-03-15 02:34:49 -0700

    Added global variables class for easy transfer of objects between activities.
    Also edited styling of XML Activities.

---
+++ app/src/main/java/Globals.java
@@ -0,0 +1,12 @@
+import com.nwhacks.ecoquest.classes.Challenge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Alvin Hon 3/15/2015.
+ */
+public class Globals {
+    public static List<Challenge> challengesInProgress = new ArrayList<Challenge>();
+    public static List<Challenge> challengesCompleted = new ArrayList<Challenge>();
+}
